0eb1d65838594786d749cfd8ccebece7e2f80037
Coma <comasocean@gmail.com>
2025-09-02 09:59:55 +0900

    Update: Gradle

---
+++ src/main/java/dev/coma/study/GradleGoodeeStudyApplication.java
@@ -0,0 +1,13 @@
+package dev.coma.study;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GradleGoodeeStudyApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GradleGoodeeStudyApplication.class, args);
+	}
+
+}
---
+++ src/test/java/dev/coma/study/GradleGoodeeStudyApplicationTests.java
@@ -0,0 +1,13 @@
+package dev.coma.study;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GradleGoodeeStudyApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
